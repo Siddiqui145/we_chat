26e48840f1abb72f7c018a6a96fc1aaec8e29b9e
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-24 15:28:42 +0530

    validations & form field updated with buttons & functions

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -8,6 +8,21 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit(){
+    final isValid = _formKey.currentState!.validate();
+
+    if (isValid){
+      _formKey.currentState!.save();
+      print(_enteredEmail);
+      print(_enteredPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,23 +48,60 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                   padding: EdgeInsets.all(16),
                   child: Form(
+                    key: _formKey,
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        TextField(
+                        TextFormField(
                           decoration: InputDecoration(
                             labelText: "Email Address",
                           ),
                           keyboardType: TextInputType.emailAddress,
                           autocorrect: false,
                           textCapitalization: TextCapitalization.none,
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty || !value.contains('@')){
+                              return 'Please enter a valid email address';
+                            }
+                            return null;
+                          },
+                          onSaved: (newValue) {
+                            _enteredEmail = newValue!;
+                          },
                         ),
-                        TextField(
+                        TextFormField(
                           decoration: InputDecoration(
                             labelText: "Password",
                           ),
                           obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty){
+                              return 'Please enter a password';
+                            }
+                            if (value.trim().length < 6){
+                              return 'Password must be at least 6 characters long!';
+                            }
+                            return null;
+                          },
+                          onSaved: (newValue) {
+                            _enteredPassword = newValue!;
+                          },
                         ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        ElevatedButton(onPressed: _submit,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).colorScheme.primaryContainer
+                        ),
+                         child: Text(_isLogin ? 'Login' : 'Signup')),
+
+                        TextButton(onPressed: () {
+                          setState(() {
+                            _isLogin = !_isLogin;
+                          });
+                        }, 
+                        child: Text(_isLogin ? 'Create an account' : 'Already have an account')),
                       ],
                   )),),
                 ),
