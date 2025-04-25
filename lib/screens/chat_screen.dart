39580536ff11b297dd426c56a3eb4dcabcad10ec
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-25 16:03:23 +0530

    push noti setup, req. & test done

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:we_chat/widgets/chat_message.dart';
 import 'package:we_chat/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNoti() async{
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();  //final notificationSettings has various options further
+    await fcm.getToken();
+    //final token =     print(token); 
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    setupPushNoti();
+  }
 
   @override
   Widget build(BuildContext context) {
