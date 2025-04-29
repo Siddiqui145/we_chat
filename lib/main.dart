98c165633049fe4698fadf6eb3626f704fe9eaf2
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-29 14:14:07 +0530

    policy issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:we_chat/firebase_options.dart';
+import '../firebase_options.dart';
 import 'package:we_chat/screens/auth_screen.dart';
 import 'package:we_chat/screens/chat_screen.dart';
 import 'package:we_chat/screens/splash_screen.dart';
